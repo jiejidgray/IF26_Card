cfaf73660f76ed53a0baf6b71e0917e8f92b23ba
zyt9749 <zhangyuetong9749@outlook.com>
2018-12-04 16:25:29 +0100

    modification -m

--- app/src/main/java/fr/utt/if26/if26_card/MainActivity.java
+++ app/src/main/java/fr/utt/if26/if26_card/MainActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
